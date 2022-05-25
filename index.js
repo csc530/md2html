bd483134edb5b6bede5f55adb2de5d6a3c7fb6b5
Christofer <77406318+csc530@users.noreply.github.com>
2022-05-25 10:25:33 -0400

    Add prompt to enter path of md file if no file or directory is provided during the program commands [args]

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 (() => {
-    const fs = require('fs')
-    const stdin = process.stdin; 
+    const fs = require('fs');
+    //TODO: make a module to more concisely get stdin values
+    const input = process.stdin.on;
     const args = process.argv.splice(2);
     console.log(args);
-    if(args.length === 0)
-    {
+    if (args.length === 0) {
         // Todo: setup prompt for file/dir
         console.log("Please enter the file or directory of markdown files to convert");
         console.log("(only .md files will be processed).\n");
-        //todo: prompt in a while to make sure it's correct
+        let path = undefined;
+        while (!path)
+            input('data', data => {
+                path = data;
+                const filetype = fs.lstatSync(path)
+                if (!(fileType.isDirectory() || fileType.isFile()))
+                    path = null;
+            });
+        arg[0] = path;
     }
     const path = args[0];
     if (typeof path !== 'string')
@@ -16,25 +24,25 @@
     const fileType = fs.lstatSync(path);
     if (fileType.isDirectory()) {
         const dir = fs.opendirSync(path);
-        let file =dir.readSync();
-        while(file){
+        let file = dir.readSync();
+        while (file) {
             convert(fs.readFileSync(`${dir.path}/${file.name}`));
         }
     }
     else if (fileType.isFile()) {
-        const file = fs.readFileSync(path,{encoding: 'utf-8'});
+        const file = fs.readFileSync(path, { encoding: 'utf-8' });
         convert(file);
     }
 
-    function convert(file){
+    function convert(file) {
         const tokens = tokenize(file);
         const ast = parse(tokens);
         const html = compile(ast);
         return html;
 
-        function tokenize(file){}
-        function parse(tokens){}
-        function compile(ast){}
+        function tokenize(file) { }
+        function parse(tokens) { }
+        function compile(ast) { }
 
     }
 })();
